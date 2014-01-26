a29c70cd65ffda05d076d10329952f81b85c7889
rajeshatuce <rajeshatuce@gmail.com>
2014-01-26 02:40:03 -0800

    Test case

---
+++ src/test/java/com/conftrackmgmnt/controller/ConfTrackMgmntControllerUnitTest.java
@@ -0,0 +1,12 @@
+package com.conftrackmgmnt.controller;
+
+import org.junit.Test;
+
+public class ConfTrackMgmntControllerUnitTest {
+	
+	@Test
+	public void testMain(){
+		System.out.println("Test");
+	}
+
+}
